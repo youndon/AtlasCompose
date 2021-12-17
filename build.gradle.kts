a785aa21656ac99ca4324ac9a1714eeba572e958
yon <bourntivoli@gmail.com>
2021-12-17 18:06:41 +0100

    17/12/21

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.6.0"
     id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
